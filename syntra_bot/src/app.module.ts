816ba7869f48f3a4012d6d1bfae339e4500ed17b
eketeUg <emmanuelugochukwuekete@gmail.com>
2025-04-21 14:05:35 +0100

    db setUp

--- syntra_bot/src/app.module.ts
+++ syntra_bot/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { SyntraBotModule } from './syntra-bot/syntra-bot.module';
 import { DexModule } from './dex/dex.module';
 import { VybeIntegrationModule } from './vybe-integration/vybe-integration.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [SyntraBotModule, DexModule, VybeIntegrationModule],
+  imports: [SyntraBotModule, DexModule, VybeIntegrationModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
